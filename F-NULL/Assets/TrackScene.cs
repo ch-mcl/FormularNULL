2100d4287e9ab0c354ed80fde257a5249c2c29cb
SFuru <mphchmcl@gmail.com>
2019-01-28 03:15:44 +0900

    update tracks

---
+++ F-NULL/Assets/TrackScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackScene : MonoBehaviour {
+	[SerializeField]
+	Color beamColor;
+
+	[SerializeField]
+	Color ambientColor;
+
+	[SerializeField]
+	Material trackMaterial;
+
+	// Use this for initialization
+	void Start () {
+		RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Flat;
+		RenderSettings.ambientSkyColor = ambientColor;
+	}
+}

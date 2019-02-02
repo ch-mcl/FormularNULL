8afe9e535c8dc92079aa8d9fb653e0d8f42a84e7
SFuru <mphchmcl@gmail.com>
2019-02-02 22:59:31 +0900

    Update Tracks Add Track Scene Controler

--- F-NULL/Assets/TrackScene.cs
+++ F-NULL/Assets/TrackScene.cs
@@ -12,9 +12,29 @@ public class TrackScene : MonoBehaviour {
 	[SerializeField]
 	Material trackMaterial;
 
+	[SerializeField]
+	[Range(0.0f, 1.0f)]
+	float colorLearp = 0.5f;
+
 	// Use this for initialization
 	void Start () {
 		RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Flat;
 		RenderSettings.ambientSkyColor = ambientColor;
 	}
+
+	void Update() {
+		Color glow;
+		Color learpedColor;
+
+		float t;
+
+		t = Mathf.PingPong(Time.time, 1.0f);
+
+		learpedColor = Color.Lerp(Color.white, Color.black, t);
+
+		glow = Color.Lerp(beamColor, learpedColor, colorLearp);
+
+		trackMaterial.SetColor("_EmissionColor", glow);
+	}
+
 }
